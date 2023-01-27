12dc134b54b20027ebb1518fb928d5c47618d004
Dmitriy <maradonna05@yandex.ru>
2023-01-27 13:26:49 +0300

    Добавьте файлы проекта.

---
+++ ARRAY_MIN_MAX_AVERAGE_SUM/ARRAY_MIN_MAX_AVERAGE_SUM.cpp
@@ -0,0 +1,44 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	int arr[100];
+	int n;
+
+	printf("Введите кол-во элементов: ");
+	scanf("%i", &n);
+
+	printf("Введите элементы:\n");
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%i", &arr[i]);
+	}
+
+	int min, max;
+	min = max = arr[0];
+	int sum = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
+
+		if (arr[i] < min)
+		{
+			min = arr[i];
+		}
+
+		sum += arr[i];
+	}
+
+	float average = (float)sum / (float)n;
+
+	printf("Минимальный элемент: %i, Максимальный элемент: %i, Сумма элементов: %i, Среднее арифметическое: %f\n", min, max, sum, average);
+
+}
